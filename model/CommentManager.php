490bff55d6d3d21dda0165faf6fd4ac1551ba2ad
phil <flipiste@free.fr>
2018-02-05 17:36:19 +0100

    supression des anciens codes qui étaient en commentaire dans Postmanager.php et CommentManager.php depuis le passage en mode objet ++ ré-indentation netbeans

--- model/CommentManager.php
+++ model/CommentManager.php
@@ -22,66 +22,42 @@ use \perou\blog\model\Manager;
 
 Class CommentManager extends Manager
 {
-	public function getComments($postId)
-	{
-		$sql = 'SELECT comment_id, comment_author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC';
-		$comments = $this->executerRequete($sql, array($postId));
+    public function getComments($postId)
+    {
+        $sql = 'SELECT comment_id, comment_author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC';
+        $comments = $this->executerRequete($sql, array($postId));
 
-	    /*$db = $this->dbConnect();
-	    $comments = $db->prepare('SELECT comment_id, comment_author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
-	    $comments->execute(array($postId));*/
+        return $comments;
+    }
 
-	    return $comments;
-	}
+    public function postComment($post_id, $comment_author, $comment_c)
+    {
+        $sql = 'INSERT INTO comments(post_id, comment_author, comment, comment_date) VALUES(:id, :author, :comment, NOW())';
+        $affectedLines = $this->executerRequete($sql, array('id' => $post_id,
+                                                                                                    'author' => htmlspecialchars($comment_author),
+                                                                                                    'comment' => nl2br(htmlspecialchars($comment_c))
+                                                                                                    ));
 
-	public function postComment($post_id, $comment_author, $comment_c)
-	{
-		$sql = 'INSERT INTO comments(post_id, comment_author, comment, comment_date) VALUES(:id, :author, :comment, NOW())';
-		$affectedLines = $this->executerRequete($sql, array('id' => $post_id,
-													        'author' => htmlspecialchars($comment_author),
-													        'comment' => nl2br(htmlspecialchars($comment_c))
-													    	));
+        return $affectedLines;
+    }
 
-	    /*$db = $this->dbConnect();
-	    $comment = $db->prepare('INSERT INTO comments(post_id, comment_author, comment, comment_date) VALUES(:id, :author, :comment, NOW())');
-	    $affectedLines = $comment->execute(array(
-	        'id' => $post_id,
-	        'author' => htmlspecialchars($comment_author),
-	        'comment' => nl2br(htmlspecialchars($comment_c))
-	    ));*/
+    public function getComment($comment_id)
+    {
+        $sql = 'SELECT comment_id, comment_author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE comment_id = ?';
+        $req = $this->executerRequete($sql, array($comment_id));
+        $comment = $req->fetch();
+        $req->closeCursor();
 
-	    return $affectedLines;
-	}
+        return $comment;
+    }
 
-	public function getComment($comment_id)
-	{
-		$sql = 'SELECT comment_id, comment_author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE comment_id = ?';
-		$req = $this->executerRequete($sql, array($comment_id));
-		$comment = $req->fetch();
-	    $req->closeCursor();
+    public function setComment($comment_id, $new_comment)
+    {
+        $sql = 'UPDATE comments SET comment = :new_comment, comment_date = NOW() WHERE comment_id = :id';
+        $affectedLines = $this->executerRequete($sql, array('new_comment' => $new_comment,
+                                                                                                    'id' => $comment_id
+                                                                                                   ));
 
-		/*$db = $this->dbConnect();
-	    $req = $db->prepare('SELECT comment_id, comment_author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE comment_id = ?');
-	    $req->execute(array($comment_id));
-	    $comment = $req->fetch();
-	    $req->closeCursor();*/
-
-	    return $comment;
-	}
-
-	public function setComment($comment_id, $new_comment)
-	{
-		$sql = 'UPDATE comments SET comment = :new_comment, comment_date = NOW() WHERE comment_id = :id';
-		$affectedLines = $this->executerRequete($sql, array('new_comment' => $new_comment,
-															'id' => $comment_id
-															));
-
-		/*$db = $this->dbConnect();
-		$comment = $db->prepare('UPDATE comments SET comment = :new_comment, comment_date = NOW() WHERE comment_id = :id');
-		$affectedLines = $comment->execute(array(
-									'new_comment' => $new_comment,
-									'id' => $comment_id
-									));*/
-		return $affectedLines;
-	}
+            return $affectedLines;
+    }
 }
--- model/PostManager.php
+++ model/PostManager.php
@@ -24,10 +24,9 @@ Class PostManager extends Manager
 {
     public function getPosts()
     {
-        /*$db = $this->dbConnect();*/
         $sql = 'SELECT post_id, post_title, post_content, post_author, DATE_FORMAT(post_creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS post_creation_date_fr FROM posts ORDER BY post_creation_date DESC LIMIT 0, 5';
         $req = $this->executerRequete($sql);
-        /*$req = $db->query('SELECT post_id, post_title, post_content, post_author, DATE_FORMAT(post_creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS post_creation_date_fr FROM posts ORDER BY post_creation_date DESC LIMIT 0, 5');*/
+        
        $postsTab = array();
         while ($newPostDatas = $req->fetch(\PDO::FETCH_ASSOC))
         {
@@ -41,12 +40,6 @@ Class PostManager extends Manager
             $sql = 'SELECT post_id, post_title, post_content, post_author, DATE_FORMAT(post_creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS post_creation_date_fr FROM posts WHERE post_id = ?';
             $req = $this->executerRequete($sql, array($postId));
             $billet = new Post($req->fetch(\PDO::FETCH_ASSOC));
-
-        /*$db = $this->dbConnect();
-        $req = $db->prepare('SELECT post_id, post_title, post_content, post_author, DATE_FORMAT(post_creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS post_creation_date_fr FROM posts WHERE post_id = ?');
-        $req->execute(array($postId));
-        $post = $req->fetch();
-        $req->closeCursor();*/
 
         return $billet;
     }
