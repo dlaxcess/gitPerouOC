c5b035e44b568787388170a29ee5c27e007ad461
phil <flipiste@free.fr>
2018-02-05 11:09:20 +0100

    PostControler.php mixé

--- controler/PostControler.php
+++ controler/PostControler.php
@@ -12,20 +12,29 @@ require_once('view/frontend/View.php');
 
 class PostControler
 {
-	private $_billet;
-	private $_comments;
+    private $_billets;
+    private $_billet;
+    private $_comments;
 
-	public function __construct()
-	{
-		$this->_billet = new PostManager;
-		$this->_comments = new CommentManager;
-	}
+    public function __construct()
+    {
+        $this->_billets = new PostManager;
+        $this->_billet = new PostManager;
+        $this->_comments = new CommentManager;
+    }
 
-	public function post()
-	{
-		$billet = $this->_billet->getPost($_GET['post_id']);
-		$comments = $this->_comments->getComments($_GET['post_id']);
-		$affichePost = new View('post');
-		$affichePost->generer(array('post' => $billet, 'comments' => $comments));
-	}
+    public function post()
+    {
+        $billet = $this->_billet->getPost($_GET['post_id']);
+        $comments = $this->_comments->getComments($_GET['post_id']);
+        $affichePost = new View('post');
+        $affichePost->generer(array('post' => $billet, 'comments' => $comments));
+    }
+    
+    public function listPosts()
+    {
+        $billets = $this->_billets->getPosts();
+        $affichePosts = new View('listPosts');
+        $affichePosts->generer(array('posts' => $billets));
+    }
 }
